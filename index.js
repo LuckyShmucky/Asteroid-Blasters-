d0bcd08a8ca34c664ec5a93b197ceb1fba2b1600
LuckyShmucky <christianluciano567@gmail.com>
2022-02-09 23:52:20 -0800

    discovered a possible way to win game, and asteroids make sound when clicked

--- index.js
+++ index.js
@@ -1,53 +1,64 @@
-
+//creating random numbers for X and Y coordinates
  function getX(){
-    return Math.floor(Math.random() * 90) + 5;
+    return Math.floor(Math.random() * 85) + 10;
     //console.log(x) 
     }
 
  function getY(){
-    return Math.floor(Math.random() * 90) + 5;
+    return Math.floor(Math.random() * 85) + 10;
     // console.log(y) 
     }
     
+    //sleep function
+    function sleep(time){
+        return new Promise(resolve =>{
+            setTimeout(resolve, time)
+        })
+    }
     
-
-
+let score = 0;
 
 //this function is creating the asteroid element and recieves its positioning
 //on the screen via two arguments: left, bottom
 function generateAsteroid(){
-   let asteroidImg = document.createElement('img');
-   asteroidImg.classList.add('asteroid');
-   asteroidImg.src = 'images/smallAsteroid.png';
-   asteroidImg.style.position= 'fixed';
-   asteroidImg.style.left = getX() + 'vw';
-   asteroidImg.style.bottom = getY() + 'vh'
-   document.getElementById('root').appendChild(asteroidImg)
-   
-   //event listener for asteroids
-   asteroidImg.addEventListener('click', function(){
-       asteroidImg.style.display = 'none'
-   })
+    let asteroidImg = document.createElement('img');
+    asteroidImg.classList.add('asteroid');
+    asteroidImg.src = 'images/smallAsteroid.png';
+    asteroidImg.style.position= 'fixed';
+    asteroidImg.style.left = getX() + 'vw';
+    asteroidImg.style.bottom = getY() + 'vh'
+    //    asteroidImg.style.animationName = 'slidein'
+    //    asteroidImg.style.animationDuration = '3s'
+    //    asteroidImg.style.
+    
+    document.getElementById('root').appendChild(asteroidImg)
+    
+    //event listener for asteroids
+    asteroidImg.addEventListener('click', function(){
+        // let explosion = new Audio('sounds/fastExplosion.wav')
+        // explosion.play()
+        asteroidImg.style.display = 'none'
+        score++
+        console.log(score)
+        if(score === 2){
+            alert('You Win!')
+        }else{console.log('L')}
+})
+
 }
 
-//sleep function
-function sleep(time){
-    return new Promise(resolve =>{
-        setTimeout(resolve, time)
-    })
-}
 
 //a helper function to generate asteroids in random locations
 async function in2Seconds(){
     await sleep(2000)
     generateAsteroid(getX(), getY())
-    console.log('hello')
+    
 }
 //same as above trying to debug
 async function in6Seconds(){
     await sleep(6000)
     generateAsteroid(getX(), getY())
-    console.log('world')
+    
 }
 
 in2Seconds()
