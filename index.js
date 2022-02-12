2e57a5fb515b4ff84dec340c0e082411c9e128af
LuckyShmucky <christianluciano567@gmail.com>
2022-02-12 13:41:44 -0800

    Successfully added a way for player to lose and win the game with proper notifications

--- index.js
+++ index.js
@@ -16,6 +16,11 @@
         })
     }
     
+    // function gameTime(time){
+    //     return new Promise (resolve =>{
+    //         setTimeout(resolve, time)
+    //     })
+    // }
 
     //creates a score to allow player to win or lose the game 
 let score = {
@@ -34,10 +39,33 @@ const startButton = document.getElementById('start')
 const guideDiv = document.getElementById('guide')
 startButton.addEventListener('click', async function(){
      guideDiv.style.display = 'none'
+     restartGame()
     in2Seconds()
     in6Seconds()
+
+    //the logic to let the player lose has to invoked again after the time has gone by
+    await sleep(6000)
+    if (score.points < 2){
+        playerLost()
+        //guideDiv.style.display = 'block'
+        
+    }
+   
 })
 
+const winCard = document.getElementById('winCard')
+const loseCard = document.getElementById('loseCard')
+
+//trying to add a way for player to lose
+ function playerLost(){
+    loseCard.style.display = 'block'
+    guideDiv.style.display = 'block'
+}
+ function playerWon(){
+    winCard.style.display = 'block'
+    guideDiv.style.display = 'block'
+
+ }
 
 //this function is creating the asteroid element and recieves its positioning
 //on the screen via two arguments: left, bottom
@@ -58,25 +86,33 @@ function generateAsteroid(){
     document.getElementById('root').appendChild(asteroidImg)
     
     //event listener for asteroids
-    asteroidImg.addEventListener('click', function(){
+    asteroidImg.addEventListener('click', async function(){
         // let explosion = new Audio('sounds/fastExplosion.wav')
         // explosion.play()
         asteroidImg.remove()
         //asteroidImg.style.display = 'none'
         score.points++
         console.log(score)
-
+      
         //here I am creating a way for the player to win the game and reset the score to 0 
-        if(score.points === 2){
-            alert('You Win!')
-            restartGame()
-            console.log(score.points)
-        }else{console.log('L')}
+        if (score.points === 2){
+            playerWon()
+            //guideDiv.style.display = 'block'
+            
+        } else{
+        // await sleep(6000)
+        // playerLost() 
+    }
+    
 })
 
 }
 
  
+// async function timeLimit(){
+//     await sleep(1000)
+//     loseCard.style.display = 'block'
+// }
 
 
 
@@ -89,22 +125,21 @@ async function in2Seconds(){
     }
     
 }
-//same as above trying to debug
+
 async function in6Seconds(){
-    await sleep(6000)
+    await sleep(4000)
     for (var i = 0; i < 1; i++){
         generateAsteroid(getX(), getY())
     }
     
 }
 
-// in2Seconds()
-// in6Seconds()
+
 
 //resets score for now
 function restartGame(){
     score.points = 0
-    guideDiv.style.display = 'block'
+    
 }
 
 
